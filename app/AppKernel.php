97fec4dd4f3a481ae2f1401d32e6bb48fadd3a98
Folow <davidgalante77@gmail.com>
2017-05-15 19:53:40 +0200

    -Création du bundle cursus

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new UTT\EtuBundle\UTTEtuBundle(),
             new UTT\CoreBundle\UTTCoreBundle(),
+            new UTT\CursusBundle\UTTCursusBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
---
+++ src/UTT/CursusBundle/UTTCursusBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace UTT\CursusBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class UTTCursusBundle extends Bundle
+{
+}
