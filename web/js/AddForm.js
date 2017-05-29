787b51795514eeb646fd277b978d7183aa2c80a4
Folow <davidgalante77@gmail.com>
2017-05-29 19:03:13 +0200

    modification du js sur l'ajout d'un element au cursus

--- web/js/AddForm.js
+++ web/js/AddForm.js
@@ -2,12 +2,62 @@
  * Created by david on 22/05/2017.
  */
 
-var AddForm=function()
+var AddForm=function($container)
 {
+    this._$container=$container;
+    this._index=this._$container.find(':input').length;
+
 
 }
 
-AddForm.prototype.addFormElement()
-{
+AddForm.prototype.addDynamicElement=function()
+    {
+            // Dans le contenu de l'attribut « data-prototype », on remplace :
+        // - le texte "__name__label__" qu'il contient par le label du champ
+        // - le texte "__name__" qu'il contient par le numéro du champ
+        this._template=this._$container.attr('data-prototype')
+            .replace(/__name__label__/g, 'Element n°' + (this.index+1))
+            .replace(/__name__/g,        this.index);
+        // On crée un objet jquery qui contient ce template
+        this._$prototype= $(this._template);
+        // On ajoute au prototype un lien pour pouvoir supprimer la catégorie
+        this.addDeleteLink(this._$prototype);
 
+        // On ajoute le prototype modifié à la fin de la balise <div>
+        this._$container.append(this._$prototype);
+
+        // Enfin, on incrémente le compteur pour que le prochain ajout se fasse avec un autre numéro
+            this._index++;
+    }
+AddForm.prototype.addDeleteLink=function ()
+    {
+    // Création  du lien
+    var $deleteLink = $('<a href="#" class="btn btn-danger">Supprimer</a>');
+
+    // Ajout du lien
+    this._$prototype.append($deleteLink);
+
+    // Ajout du listener sur le clic du lien pour effectivement supprimer la catégoriea
+        var addform=this;
+    $deleteLink.click(function (e) {
+        addform._$prototype.remove();
+
+        e.preventDefault(); // évite qu'un # apparaisse dans l'URL
+        return false;
+    });
 }
+AddForm.prototype.setPrototype=function($prototype)
+{
+    this._$prototype=$prototype;
+}
+
+AddForm.prototype.getIndex=function()
+{
+    return this._index;
+}
+AddForm.prototype.getPrototype=function()
+{
+    return this._$prototype;
+}
+
+
