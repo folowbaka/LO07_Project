1caee60a2a953c0e61279f21db0c17594709e533
rajitha kosgoda gan <rajitha kosgoda gan@rajitha>
2017-06-09 15:38:59 +0200

    debut parsage

--- src/UTT/CSVBundle/Controller/CSVController.php
+++ src/UTT/CSVBundle/Controller/CSVController.php
@@ -6,9 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use UTT\CSVBundle\Entity\CSV;
 use UTT\CSVBundle\Form\CSVType;
-use Symfony\Component\Serializer\Serializer;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
-use Symfony\Component\Serializer\Encoder\CsvEncoder;
+use UTT\EtuBundle\Entity\Etudiant;
+use UTT\EtuBundle\Entity\Admission;
+use UTT\EtuBundle\Entity\Filliere;
 
 class CSVController extends Controller
 {
@@ -19,31 +19,81 @@ class CSVController extends Controller
     }
     public function uploadAction(Request $request)
     {
+        $repositoryEtudiant = $this
+            ->getDoctrine()
+            ->getManager()
+            ->getRepository('UTTEtuBundle:Etudiant');
+        $repositoryAdmission = $this
+            ->getDoctrine()
+            ->getManager()
+            ->getRepository('UTTEtuBundle:Admission');
+        $repositoryFilliere = $this
+            ->getDoctrine()
+            ->getManager()
+            ->getRepository('UTTEtuBundle:Filliere');
+        $em = $this->getDoctrine()->getManager();
         $csv= new CSV();
         $form   = $this->get('form.factory')->create(CSVType::class, $csv);
-        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {            
             $data=$form['file']->getData();
-            $array= null;
+            $donnee= null;
             $file=  fopen($data->getrealpath(), "r");
-            while (($donnee = fgetcsv($file, 1000, ";")) !== FALSE) {
-                 $array[] = $donnee;
-                     }
-            dump($array[]);
-            fclose($file);
+            while (($donnee_ligne = fgetcsv($file, 1000, ";")) !== FALSE)
+                    {
+                        $donnee[] = $donnee_ligne;
+                     } 
+            fclose($file);                    
             
-            $em = $this->getDoctrine()->getManager();
+            
+            if($donnee[0][0]=="ID")
+            {
+                $idetu=$donnee[0][1];
+                 $etudiant=$repositoryEtudiant->find($idetu);
+
+                 if($etudiant==null)
+                 {
+                     $etudiantobjet=new Etudiant();
+                     $etudiant=array();
+                     for ($i=0;$i<=4;$i++)
+                     {
+                         array_push($etudiant,$donnee[$i][1]);
+                     }
+                    dump($etudiant);
+                    $etudiantobjet=new Etudiant();
+                    dump(gettype($etudiantobjet));
+                    $admission=new Admission();
+                    $admission=$repositoryAdmission->findOneByNom($etudiant[3]);
+                    dump(gettype($admission));
+                    $filliere=$repositoryFilliere->findOneByNom($etudiant[4]);
+                    $etudiantobjet->setIdEtudiant($etudiant[0]);
+                    $etudiantobjet->setNom($etudiant[1]);
+                    $etudiantobjet->setPrenom($etudiant[2]);
+                    $etudiantobjet->setAdmission($admission);
+                    $etudiantobjet->setFilliere($filliere);
+                    dump($etudiant);
+                    $em->persist($etudiantobjet);
+                    $em->flush();
+                 }
+                 else
+                     {
+                     
+                     }
+                     
+                 }
+            }
+                           
             $em->persist($csv);
             $em->flush();
             
 
             $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
             //return $this->redirectToRoute('utt_csv_view',array('idcsv'=>$csv->getId()));
-        }
+        
         
 
 
 
         return $this->render('UTTCSVBundle:CSV:upload.html.twig',array('form' => $form->createView()
         ));           
-    }
+        }
 }
